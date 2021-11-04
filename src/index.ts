36fc8695eca5e6c0a4794aef4211746460dce310
xychen <xychen@listenai.com>
2021-11-04 17:23:21 +0800

    Migrate to zstd distribution

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,16 @@
-import { platform } from 'os';
 import { join } from 'path';
 import { promisify } from 'util';
 import { execFile as _execFile } from 'child_process';
 import { Binary } from '@binary/type';
 
-const PREFIX = (() => {
-  switch (platform()) {
-    case 'win32': return 'bin_x86-64';
-    case 'darwin': return 'xz/5.2.5';
-    default: return 'usr';
-  }
-})();
-
-const HOME = join(__dirname, '..', 'binary');
 const execFile = promisify(_execFile);
 
+export const HOME = join(__dirname, '..', 'binary');
+
 export default <Binary>{
-  homeDir: join(HOME, PREFIX),
+  homeDir: HOME,
 
-  binaryDir: platform() == 'win32' ? join(HOME, PREFIX) : join(HOME, PREFIX, 'bin'),
-
-  env: {},
+  binaryDir: join(HOME, 'bin'),
 
   async version() {
     const { stdout } = await execFile(join(this.binaryDir, 'xz'), ['--version']);
--- src/install.ts
+++ src/install.ts
@@ -1,20 +1,13 @@
-import * as download from 'download';
+import download from '@xingrz/download2';
 import { rm } from 'fs/promises';
-import { platform } from 'os';
-import { join } from 'path';
+import { HOME } from './index';
 
-const PREFIX = 'https://cdn.iflyos.cn/public/lisa-binary/xz/';
+const PACKAGE = 'xz';
+const VERSION = '5.2.5';
 
-const SUFFIX = (() => {
-  switch (platform()) {
-    case 'win32': return 'windows.zip';
-    case 'darwin': return 'darwin_amd64.tar.gz';
-    default: return 'linux_amd64.tar.gz';
-  }
-})();
+const NAME = `${PACKAGE}-${VERSION}-${process.platform}_${process.arch}.tar.zst`;
 
-const NAME = `xz-5.2.5-${SUFFIX}`;
-const HOME = join(__dirname, '..', 'binary');
+const URL = `https://cdn.iflyos.cn/public/lisa-binary/${PACKAGE}/${NAME}`;
 
 (async () => {
 
@@ -23,7 +16,7 @@ const HOME = join(__dirname, '..', 'binary');
   } catch (e) {
   }
 
-  await download(`${PREFIX}${NAME}`, HOME, {
+  await download(URL, HOME, {
     extract: true,
   });
 
